c292ea4c7d5567ec44767255710920b2eb474acb
micahcourey <micahjcourey@gmail.com>
2015-09-30 16:54:29 -0700

    add to API Factory

--- services/ApiFactory.js
+++ services/ApiFactory.js
@@ -1,8 +1,10 @@
 stayCation.factory('ApiFactory', function ApiFactory() {
   var factory = {};
 
+  // load the latest SKD file
   js.src = "//connect.facebook.net/en_US/sdk.js";
 
+  // Initialize facebook API
   $window.fbAsyncInit = function() {
     FB.init({
       appId: 'e86a706dcf6cb19',
