35d68b07622ac790a47c9203a90da67212b990ce
Ashlin <ashlinaronin@gmail.com>
2015-09-30 12:04:19 -0700

    Get voice recognition working in console

--- controllers/WebSpeechCtrl.js
+++ controllers/WebSpeechCtrl.js
@@ -7,32 +7,13 @@ stayCation.controller('WebSpeechCtrl', function WebSpeechCtrl($scope) {
   $scope.interim = "";
   $scope.chunks = [];
   $scope.sentenceLength = 0;
-
-  $scope.displayTranscript = function (transcript) {
-    $scope.transcript = transcript;
-  };
-
-  $scope.renderInterimResults = function(sentence) {
-    $scope.interim = sentence;
-  }
-
-  $scope.msgToPage = function(msg) {
-    $scope.message = msg;
-  }
-
-  $scope.renderPage = function() {
-    $scope.interim = "";
-  }
+  $scope.final = "";
+  $scope.recognizing = false;
 
   $scope.splitChunks = function(transcript) {
     $scope.chunks = transcript.split(/\s/);
-    $scope.sentenceLength = words.length;
+    $scope.sentenceLength = $scope.chunks.length;
   }
-
-
-
-
-
 
 
 
@@ -49,7 +30,7 @@ stayCation.controller('WebSpeechCtrl', function WebSpeechCtrl($scope) {
       recognition.onstart = function() {
         console.log("i'm on start");
         $('#talk-now').html('Talk now pls');
-        ga('send', 'event', 'userAction', 'gaveMicAccess');
+        $scope.recognizing = true;
       };
 
       // Do these things when the user has finished talking
@@ -59,26 +40,35 @@ stayCation.controller('WebSpeechCtrl', function WebSpeechCtrl($scope) {
         var sentence = event.results[0][0].transcript;
         var final = event.results[0].isFinal;
 
+        // Display interim results
         if (!final) {
-          renderInterimResults(sentence);
-          ga('send', 'event', 'userAction', 'StartedTalking');
+          $scope.interim = sentence;
+          console.log(sentence);
         } else {
-          splitChunks(sentence);
-          ga('send', 'event', 'result', sentence);
-          ga('send', 'event', 'userAction', 'finishedTalking');
+          $scope.splitChunks(sentence);
+          $scope.final = sentence;
+          console.log("final is " + sentence);
+
+          // done, stop
+          if ($scope.recognizing) {
+            recognition.stop();
+            console.log("i stopped");
+          }
         }
       };
 
       recognition.onerror = function(event) {
         console.log("i'm on error");
         if (event.error === "not-allowed") {
-          msgToPage("I'm sorry, what was that?");
-          ga('send', 'event', 'userAction', 'deniedAccess');
+          $scope.message = "I'm sorry, what was that?";
         } else {
-          msgToPage("I'm sorry!! Something went wrong.");
-          ga('send', 'event', 'userAction', 'error');
-          Rollbar.error('Recognition_Error', event);
+          $scope.message = "I'm sorry!! Something went wrong.";
         }
+      };
+
+      recognition.onend = function() {
+        $scope.recognizing = false;
+        // do more in here
       };
 
       // Start recognition after defining event handlers
@@ -88,10 +78,8 @@ stayCation.controller('WebSpeechCtrl', function WebSpeechCtrl($scope) {
       console.log("no speech recognition");
 
       // webkitSpeechRecognition not found
-      msgToPage("Please use the latest version of Google Chrome.");
-      window.setTimeout(function() {
-        ga('send', 'event', 'userAction', 'unsupported_browser');
-      }, 1000);
+      $scope.message = "Please use the latest version of Google Chrome.";
+      window.setTimeout(function() {}, 1000);
     };
 
   }); // end document ready
