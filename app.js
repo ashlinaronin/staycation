a1191b7c02a496a0ca9263a809faebd37251f259
Sam Martinez <sammartinez19@gmail.com>
2015-09-30 10:39:21 -0700

    basic skeleton of app

---
+++ app.js
@@ -0,0 +1,10 @@
+var stayCation = angular.module('stayCation', ['ui.router']);
+
+stayCation.config(function($stateProvider, $urlRouterProvider) {
+
+  //Path for homepage
+  $stateProvider.state('home', {
+    url:"",
+    templateUrl:"partials/home.html"
+  });
+});
