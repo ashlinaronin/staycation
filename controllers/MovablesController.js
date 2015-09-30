aec87a856cf4c8ac2f912b2af1e6fc9b2451bfea
Kyle Pratuch <kyle.pratuch@gmail.com>
2015-09-30 15:04:35 -0700

    commit before sam plugs in his laptop

--- controllers/MovablesController.js
+++ controllers/MovablesController.js
@@ -68,7 +68,85 @@ stayCation.controller('MovablesCtrl', function MovablesCtrl($scope, $stateParams
         myState.valid = false;
       }
     }, true);
+
+    canvas.addEventListener('mousemove', function(e) {
+      if (myState.dragging){
+        var mouse = myState.getMouse(e);
+        //Save offset and use it here so we can drag from where we click
+        myState.selection.x = mouse.x - myState.dragoffx;
+        myState.selection.y = mouse.y - myState.dragoffy;
+        myState.valid = false;
+      }
+    }, true);
+
+    canvas.addEventListener('mouseup', function(e) {
+      myState.dragging = false;
+    }, true);
+
+    canvas.addEventListener('dblclick', function(e) {
+      var mouse = myState.getMouse(e);
+      myState.addShape(new Shape(mouse.x - 10, mouse.y -10, 20, 20, 'rgba(0,255,0,.6)'));
+    }, true);
+
+    this.selectionColor = '#CC0000';
+    this.selectionWidth = 2;
+    this.interval = 30;
+    setInterval(function() {myState.draw(); }, myState.interval);
   }
 
+  CanvasState.prototype.draw = function() {
+    if (!this.valid) {
+      var ctx = this.ctx;
+      var shapes = this.shapes;
+      this.clear();
 
+      //Add things you want draw in the background all the time here
+
+      var l = shapes.length;
+      for (var i = 0; i < l; i++) {
+        var shape = shapes[i];
+        //Skip drawing of elements that have been moved off the screen:
+        if (shape.x > this.width || shape.y > this.height || shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
+        shapes[i].draw(ctx);
+      }
+
+      //draw selection
+      if (this.selection != null) {
+        ctx.strokeStyle = this.selectionColor;
+        ctx.lineWidth = this.selectionWidth;
+        var mySel = this.selection;
+        ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
+      }
+
+      //Add things you want drawn on top all the time here
+
+      this.valid = true;
+    }
+  }
+
+  //Creates an object with x and y defined, set to mouse position relative to canvas.
+  //Being very accurate requires that you worry about padding and borders!
+  CanvasState.prototype.getMouse = function(e) {
+    var element = this.canvas, offsetX = 0, offsetY = 0, mx, my;
+
+    //Comput the total offset
+    if(element.offsetParent !== undefined){
+      do{
+        offsetX += element.offsetLeft;
+        offsetY += element.offsetTop;
+      } while ((element = element.offsetparent));
+    }
+
+    offsetX += this.stylePaddingLeft + this.styleBorderLeft + this.htmlLeft;
+    offsetY += this.stylePaddingTop + this.styleBorderTop + this.htmlTop;
+
+    mx = e.pageX - offsetX;
+    my = e.pageY - offsetY;
+
+    //return a simple JS object with x and y defined
+    return {x: mx, y: my};
+  }
+
+  var s = new CanvasState(document.getElementById('canvas'));
+  s.addShape(new )
 })
