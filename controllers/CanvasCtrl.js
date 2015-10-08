8b6b91ee8d71cd1d12e11b811bfe09938554ae09
Ashlin <ashlinaronin@gmail.com>
2015-10-08 09:23:25 -0700

    Temporarily disable tracking features and resolve merge conflicts

--- controllers/CanvasCtrl.js
+++ controllers/CanvasCtrl.js
@@ -5,10 +5,15 @@ stayCation.controller('CanvasCtrl', function CanvasCtrl($scope, ImageFactory, Sa
   $scope.ImageFactory = ImageFactory;
 
 
-  $scope.tracker = null;
-  $scope.video = null;
-  $scope.canvas = null;
-  $scope.context = null;
+  // $scope.tracker = null;
+  // $scope.video = null;
+  // $scope.canvas = null;
+  // $scope.context = null;
+
+  SaveFactory.tracker = null;
+  SaveFactory.video = null;
+  SaveFactory.canvas = null;
+  SaveFactory.context = null;
 
   //Movable "prop" images on canvas.
 
@@ -266,17 +271,18 @@ stayCation.controller('CanvasCtrl', function CanvasCtrl($scope, ImageFactory, Sa
   //   }
   // }
 
-  // start document ready
-  angular.element(document).ready(function() {
-
-    // init();
 
 
-    var errorCallback = function(e) {
+
+
+
 
 
   angular.element(document).ready(function()
   {
+
+    // init();
+
     var errorCallback = function(e)
     {
 
@@ -284,71 +290,58 @@ stayCation.controller('CanvasCtrl', function CanvasCtrl($scope, ImageFactory, Sa
     };
 
 
-    navigator.getUserMedia({video: true, audio: true}, function(localMediaStream)
-    {
-      $scope.video = document.getElementById('video');
-      $scope.canvas = document.getElementById('canvasVid');
-      $scope.context = $scope.canvas.getContext('2d');
-
 
     navigator.getUserMedia({video: true, audio: true}, function(localMediaStream) {
-      // var video = document.querySelector('video');
-      // var canvas = document.getElementById('canvasVid');
-      // var context = canvas.getContext('2d');
-
       SaveFactory.video = document.querySelector('video');
       SaveFactory.canvas = document.getElementById('canvasVid');
       SaveFactory.context = SaveFactory.canvas.getContext('2d');
-      // console.log("$scope.context is ");
-      // console.dir($scope.video);
+      // console.dir(SaveFactory.context);
 
-
-      $scope.video.src = window.URL.createObjectURL(localMediaStream);
+      SaveFactory.video.src = window.URL.createObjectURL(localMediaStream);
 
 
       //instantiate the constructor which passes the target we want to detect
-      $scope.tracker = new tracking.ObjectTracker("face");
-      // console.dir(tracker);
-      $scope.tracker.setInitialScale(4);
-      $scope.tracker.setStepSize(2);
-      $scope.tracker.setEdgesDensity(0.1);
+      // SaveFactory.tracker = new tracking.ObjectTracker("face");
+      // SaveFactory.tracker.setInitialScale(4);
+      // SaveFactory.tracker.setStepSize(2);
+      // SaveFactory.tracker.setEdgesDensity(0.1);
 
       SaveFactory.videoReady = true;
 
-      video.addEventListener('play', function() {
+      SaveFactory.video.addEventListener('play', function() {
         // Every 33 milliseconds copy the video image to the canvas
         setInterval(function() {
 
           // maybe draw bg here
-
-          SaveFactory.context.drawImage(video, 0, 0, 320, 240);
+          // console.dir(SaveFactory.context);
+          SaveFactory.context.drawImage(SaveFactory.video, 0, 0, 320, 240);
 
 
         }, 1000 / 24);
-          var currentCanvasState = CanvasState(SaveFactory.canvas);
+          var currentCanvasState = new CanvasState(SaveFactory.canvas);
           currentCanvasState.draw();
       }, false);
     }, errorCallback);
       //read the canvas pixels with our tracker, let the camera run
-      tracking.track('#video', $scope.tracker, { camera: true });
+      // tracking.track('#video', SaveFactory.tracker, { camera: true });
 
 
       //listen for track events
-      $scope.tracker.on('track', function(event)
-      {
-        console.dir(event);
-        //clears the tracking rectangle after each event
-        $scope.context.clearRect(0, 0, canvas.width, canvas.height);
-
-
-        //foreach rectnagle event (times that the rect finds a face)
-        event.data.forEach(function(rect) {
-          // console.dir(context);
-          //we're passing in video and giving the coordinates of where we want to draw on the canvas
-          $scope.context.drawImage(video, rect.x, rect.y, canvas.width, canvas.height, rect.x, rect.y, rect.width, rect.height);
-        });
-        // $scope.$apply();
-      });
+      // SaveFactory.tracker.on('track', function(event)
+      // {
+      //   console.dir(event);
+      //   //clears the tracking rectangle after each event
+      //   SaveFactory.context.clearRect(0, 0, canvas.width, canvas.height);
+      //
+      //
+      //   //foreach rectnagle event (times that the rect finds a face)
+      //   event.data.forEach(function(rect) {
+      //     // console.dir(context);
+      //     //we're passing in video and giving the coordinates of where we want to draw on the canvas
+      //     SaveFactory.context.drawImage(video, rect.x, rect.y, canvas.width, canvas.height, rect.x, rect.y, rect.width, rect.height);
+      //   });
+      //   // $scope.$apply();
+      // });
 
       // video.addEventListener('play', function()
       // {
@@ -362,18 +355,5 @@ stayCation.controller('CanvasCtrl', function CanvasCtrl($scope, ImageFactory, Sa
   }); // end document ready
 
 
-    }, errorCallback);
-
-
-  }); // end document ready
-
-//   window.onload = function()
-//   {
-//
-//
-//
-//
-//
-// };
 
 }); // end controller
